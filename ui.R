5d9fd5156f4e11c9f199de93a3a6508a7f19327c
Alex Alspaugh <alex98@uw.edu>
2017-03-06 23:16:34 -0800

    Add updated server and ui

--- ui.R
+++ ui.R
@@ -1,35 +1,70 @@
+# Adding libraries 
 library(ggplot2)
-library(dplyr)
 library(tm)
 library(SnowballC)
 library(wordcloud)
 library(RColorBrewer)
+library(shinythemes)
+library(plotly)
+library(dplyr)
 
-#Creating UI
-my.ui <- fluidPage(
-  
-  # Application title
-  titlePanel("Word Cloud"),
-  
-  sidebarLayout(
-    # Sidebar with a slider and selection inputs
-    sidebarPanel(
-      
-      sliderInput("freq",
-                  "Minimum Frequency:",
-                  min = 50,  max = 1000, value = 100),
-      sliderInput("max",
-                  "Maximum Number of Words:",
-                  min = 1,  max = 300,  value = 100)
-    ),
+# Creates the UI
+ui = fluidPage(
+  # Selects UI theme
+  theme = shinytheme("united"),
+  # Creates NavBar layout
+  navbarPage(
+    "Trump's Tweets",
+    tabPanel("Word Analysis",
+             # Creates side panel 
+             sidebarPanel(
+               # Min Freq Slider Widget
+               sliderInput("freq",
+                           "Minimum Frequency",
+                           min = 50,  max = 1000, value = 100),
+               # Max words Slider widget
+               sliderInput("max",
+                           "Maximum Number of Words",
+                           min = 1,  max = 300,  value = 100)
+             ),
     
-    # Show Word Cloud
+    # Creates Main Panel
     mainPanel(
-      plotOutput("plot", width = "100%")
-    )
+      tabsetPanel(
+        # Creates Word Cloud Tab
+        tabPanel("Word Cloud",
+                 h3('Trump\'s Tweets Word Frequency Cloud'),
+                 p('The purpose of this section is to look at the',
+                 em("word choice"), 'and the', em('frequency'), 
+                 'of that word choice with regards to over', strong('7,000'), 'of Donald Trump\'s', 
+                 strong('tweets.'),
+                 'This is displayed by a', em('word cloud,'), ' 
+                 the words are colorized for visual diferentiation and
+                 will display as larger if they are more frequently used.'),
+                 p('', strong('Functionalities:'), 'You are able to 
+                   filter for the minimum amount of times a word appears in set of tweets 
+                   as well as filter for the maximum amount of words in the cloud via the
+                   widgets on the side.'),
+                 plotOutput("plot.cloud", width = "100%"),
+                 p('The data set for both the cloud and the plot was derived from - https://www.crowdbabble.com/blog/the-11-best-tweets-of-all-time-by-donald-trump/')
+        ),
+        # Creates Frequency Plot Tab
+        tabPanel("Frequency Plot",
+                 h3("Trump\'s Tweets Word Frequency Bar Plot"),
+                 p('The purpose of this section is too look at the most frequnently 
+                   occuring words in Trump\'s tweets and their frequency.'),
+                 p('', strong('Functionalities:'), 'You are able to 
+                   hover over each bar of the graph to get specific information.'),
+                 plotlyOutput("graph"))
+      )
+     )
+    ),
+    # Tab Panel Test
+    tabPanel("Navbar 2")
   )
+  
   
 )
 
 # Calls UI
-shinyUI(my.ui)
+shinyUI(ui)
