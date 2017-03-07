a99837b9efcc163ce18221dac459db5d6162b255
Sahana Vishwanath <1tbhd@D-10-157-79-137.dhcp4.washington.edu>
2017-03-07 00:05:30 -0800

    Adds progress on server.R and ui.R

--- server.R
+++ server.R
@@ -7,54 +7,89 @@ library(shinythemes)
 library(plotly) 
 library(dplyr)
 
+#Loads data sets for analysis from data folder
 t.data.map <- read.csv("data/trump.state.map.csv")
 c.data.map <- read.csv("data/clinton.state.map.csv")
 trump.location <- read.csv("data/trump.state.freq.csv")
 clinton.location <- read.csv("data/clinton.state.freq.csv")
 
+#Takes out unnecessary column in both trump.location csv and clinton.location csv
 trump.location$X <- NULL
 clinton.location$X <- NULL
 
+#Renames the names of the trump.location and clinton.location columns to make them more readable
 colnames(trump.location) <- c("State", "Number of Tweets")
 colnames(clinton.location) <- c("State", "Number of Tweets")
 
+#Defines a new server variable for application named my.server
 my.server <- function(input, output){
-  output$trump.map <- renderPlot({
+  
+  #Creates a map of the United States displaying the range of the number of tweets about Donald Trump per state
+  output$trump.map <- renderPlotly({
+    
+    #Creates ggplot map
     p <- ggplot(data = t.data.map) + 
-      geom_polygon(aes(x = long, y = lat, group = group, fill = frequency)) +
-      coord_quickmap() +
+      geom_polygon(aes(x = long, y = lat, group = group, fill = frequency, text = region)) +
+      coord_quickmap() 
       #scale_fill_distiller(name="???", palette = "Set2", na.value = "grey50") +
-      labs(title="Trump")
-    p
+      #labs(title="Trump")
+    
+    #Uses plotly to add interaction to map
+    p <- ggplotly(p)
+    
+    #Returns map
+    return(p)
   })
   
-  output$trump.info <- renderPrint({
-    return(input$click.key1)
+  #Returns description of the Donald Trump state map
+  output$plot1.description <- renderPrint({
+    return("This ")
   })
   
+  #Returns a table displaying the information shown on the Donald Trump map; Contains two columns - a State column and a 
+  #number of tweets column
   output$t.per.state.table <- renderDataTable({
     return(trump.location)
   })
   
-  output$clinton.map <- renderPlot({
+  #Returns description of the Donald Trump table
+  output$table1.description <- renderPrint({
+    return("result")
+  })
+  
+  #Creates a map of the United States displaying the range of the number of tweets about Hillary Clinton per state
+  output$clinton.map <- renderPlotly({
     
+    #Creates ggplot map
     p <- ggplot(data = c.data.map) + 
-      geom_polygon(aes(x = long, y = lat, group = group, fill = frequency)) +
-      coord_quickmap() +
+      geom_polygon(aes(x = long, y = lat, group = group, fill = frequency, text = region)) +
+      coord_quickmap() 
       #scale_fill_distiller(name="???", palette = "Set2", na.value = "grey50") +
-      labs(title="Clinton")
+      #labs(title="Clinton")
     
-    p
+    #Uses plotly to add interaction to map
+    p <- ggplotly(p)
+    
+    #Returns map
+    return(p)
   })
   
-  output$clinton.info <- renderPrint({
-    return(input$click.key2)
+  #Returns description of the Hillary Clinton state map
+  output$plot2.description <- renderPrint({
+    return("result")
   })
   
+  #Returns a table displaying the information shown on the Hillary Clinton map; Contains two columns - a State column and a 
+  #number of tweets column
   output$c.per.state.table <- renderDataTable({
     return(clinton.location)
   })
   
+  #Returns description of the Hillary Clinton table
+  output$table2.description <- renderPrint({
+    return("result")
+  })
 }
 
+#Creates the server
 shinyServer(my.server)
--- ui.R
+++ ui.R
@@ -7,20 +7,42 @@ library(shinythemes)
 library(plotly) 
 library(dplyr)
 
+#Loads data sets for analysis from data folder
 t.data.map <- read.csv("data/trump.state.map.csv")
 c.data.map <- read.csv("data/clinton.state.map.csv")
 trump.location <- read.csv("data/trump.state.freq.csv")
 clinton.location <- read.csv("data/clinton.state.freq.csv")
 
+#Defines new UI variable for application named my.ui
 my.ui <- fluidPage(
+  
+  #Sets theme for shiny app as 'united'
   theme = shinythemes::shinytheme("united"),
-  navbarPage("Comparing Number of Tweets per State between Donald Trump and Hillary Clinton Pre-Election",
-    tabPanel('Tweets about Trump State Map', plotOutput('trump.map', click = 'click.key1'), textOutput('plo1t.description'), verbatimTextOutput('trump.info')),
-    tabPanel('Tweets about Clinton State Map', plotOutput('clinton.map', click = 'click.key2'), textOutput('plot2.description'), verbatimTextOutput('clinton.info')),
-    tabPanel('Tweets about Trump per State Table', dataTableOutput('t.per.state.table'), textOutput('table1.description')),
-    tabPanel('Tweets about Clinton per State Table', dataTableOutput('c.per.state.table'), textOutput('table2.description'))
+  
+  #Creates page with navigation bar
+  navbarPage("Comparing Number of Tweets Mentioning Presidential Candidates per State",
     
+    #Creates tab containing a map plot named 'Tweets about Trump per State Map'; includes click for interaction and interactive information; inlcudes textual description of map
+    tabPanel('Tweets about Trump per State Map', 
+             plotlyOutput('trump.map', height = "550px",width = "1000px"), 
+             textOutput('plot1.description')),
+    
+    #Creates tab containing a table named 'Tweets about Trump per State Table'; includes textual description
+    tabPanel('Tweets about Trump per State Table', 
+             dataTableOutput('t.per.state.table'), 
+             textOutput('table1.description')),
+    
+    #Creates tab containing a map plot named 'Tweets about Clinton per State Map'; includes click for interaction and interactive information; inlcudes textual description of map
+    tabPanel('Tweets about Clinton per State Map', 
+             plotlyOutput('clinton.map', height = "550px",width = "1000px"), 
+             textOutput('plot2.description')),
+    
+    #Creates tab containing a table named 'Tweets about Clinton per State Table'; includes textual description
+    tabPanel('Tweets about Clinton per State Table', 
+             dataTableOutput('c.per.state.table'), 
+             textOutput('table2.description'))
   )
 )
 
+#Creates the UI
 shinyUI(my.ui)
