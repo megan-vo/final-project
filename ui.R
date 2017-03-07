c00a995d877f08cda20ff8796cbe623b5cfb479a
Aneesha Nanda <aneeshan@msn.com>
2017-03-07 00:03:52 -0800

    Adds working navbar and theme

--- ui.R
+++ ui.R
@@ -7,39 +7,47 @@ library(RColorBrewer)
 library(shinythemes)
 library(plotly)
 
+# WHERE TO ADD THAT + THEME_MINMAL()??
+# ARE TABS WITHIN NAVBAR OK?
+# WHY ISNT TEXT FORMATTING WORKING
+# ADD DESCRIPTIONS TO TABLE
+# ADD COMMENTS
+
 # Defines a new 'my.ui' variable with a 'fluidPage' layout to respond to changing data
 my.ui <- fluidPage(  
-  theme = shinythemes::shinytheme("united"), 
-  navbarPage("Navbar",
-    
-    
-    # Creates a 'titlePanel' with a bold title and sets browser title
-      titlePanel(strong("Clinton vs. Trump: Whose Tweets are More Popular?")),  
-    
-    # Layout the page in two columns
-    sidebarLayout(   
-      
-      # Specifies content for the 'sidebarPanel'
-      sidebarPanel( 
-        
-        p("this is my description")
-        
   
+  # Selects UI theme
+  theme = shinytheme("united"), 
+  
+  # Creates NavBar layout
+  navbarPage(
+    "Navbar",
+      
+    tabPanel("Tweet Popularity Analysis", 
+        
+        # Creates side panel
+        sidebarPanel(
+          
+          # Adds Description  
+          p("This plot shows the number of", strong("retweets (x-axis)"), "and the number of", strong("favorites (y-axis)"), "specific tweets received from the Twitter accounts of Donald Trump and Hillary Clinton. The tweets that are accounted for in this plot are from January 2016 through September 2016.", em("Hover over the plot to see the candidate, the exact number of retweets, and the exact number of favorites."))
+      
       ),
         
-        # Specifies content for main column  
+        # Creates Main Panel
         mainPanel( 
           
-          # Creates tabs for user to view different forms of data either in scatter plot or table format
           tabsetPanel(type = "tabs", 
                       tabPanel("Plot", plotlyOutput("tweets.plot")),
+                      
                       tabPanel("Table", tableOutput("tweet.stats.table"), tableOutput("clinton.table"), tableOutput("trump.table"))
-  
+            
           )
-        )
       )
     )
   )
+)
+  
+  
   
   # Creates a new 'shinyUI()' to be used in the 'shinyApp()'
   shinyUI(my.ui)
