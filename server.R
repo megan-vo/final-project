4a0638a5af4b305632ff37596909ad19f8fff475
Sahana Vishwanath <1tbhd@D-10-157-79-137.dhcp4.washington.edu>
2017-03-06 19:41:29 -0800

    Deletes unnecessary csv files from data folder; Adds required csv files; Adds progress on both server.R and ui.R

--- server.R
+++ server.R
@@ -1,234 +1,58 @@
-library("dplyr")
-library("ggplot2")
-library("shiny")
+library(ggplot2) 
+library(tm) 
+library(SnowballC) 
+library(wordcloud) 
+library(RColorBrewer) 
+library(shinythemes) 
+library(plotly) 
+library(dplyr)
 
-#first, filter user_location for rows that only have state names as location
-alabama.tweets <- all.tweets %>% 
-  filter(user_location == "Alabama")
+t.data.map <- read.csv("data/trump.state.map.csv")
+c.data.map <- read.csv("data/clinton.state.map.csv")
+trump.location <- read.csv("data/trump.state.freq.csv")
+clinton.location <- read.csv("data/clinton.state.freq.csv")
 
-alaska.tweets <- all.tweets %>% 
-  filter(user_location == "Alaska")
+trump.location$X <- NULL
+clinton.location$X <- NULL
 
-arizona.tweets <- all.tweets %>% 
-  filter(user_location == "Arizona")
-
-arkansas.tweets <- all.tweets %>% 
-  filter(user_location == "Arkansas")
-
-california.tweets <- all.tweets %>% 
-  filter(user_location == "California")
-
-colorado.tweets <- all.tweets %>% 
-  filter(user_location == "Colorado")
-
-connecticut.tweets <- all.tweets %>% 
-  filter(user_location == "Connecticut")
-
-delaware.tweets <- all.tweets %>% 
-  filter(user_location == "Delaware")
-
-florida.tweets <- all.tweets %>% 
-  filter(user_location == "Florida")
-
-georgia.tweets <- all.tweets %>% 
-  filter(user_location == "Georgia")
-
-hawaii.tweets <- all.tweets %>% 
-  filter(user_location == "Hawaii")
-
-idaho.tweets <- all.tweets %>% 
-  filter(user_location == "Idaho")
-
-illinois.tweets <- all.tweets %>% 
-  filter(user_location == "Illinois")
-
-indiana.tweets <- all.tweets %>% 
-  filter(user_location == "Indiana")
-
-iowa.tweets <- all.tweets %>% 
-  filter(user_location == "Iowa")
-
-kansas.tweets <- all.tweets %>% 
-  filter(user_location == "Kansas")
-
-kentucky.tweets <- all.tweets %>% 
-  filter(user_location == "Kentucky")
-
-louisiana.tweets <- all.tweets %>% 
-  filter(user_location == "Louisiana")
-
-maine.tweets <- all.tweets %>% 
-  filter(user_location == "Maine")
-
-maryland.tweets <- all.tweets %>% 
-  filter(user_location == "Maryland")
-
-massachusetts.tweets <- all.tweets %>% 
-  filter(user_location == "Massachusetts")
-
-michigan.tweets <- all.tweets %>% 
-  filter(user_location == "Michigan")
-
-minnesota.tweets <- all.tweets %>% 
-  filter(user_location == "Minnesota")
-
-mississippi.tweets <- all.tweets %>% 
-  filter(user_location == "Mississippi")
-
-missouri.tweets <- all.tweets %>% 
-  filter(user_location == "Missouri")
-
-montana.tweets <- all.tweets %>% 
-  filter(user_location == "Montana")
-
-nebraska.tweets <- all.tweets %>% 
-  filter(user_location == "Nebraska")
-
-nevada.tweets <- all.tweets %>% 
-  filter(user_location == "Nevada")
-
-new.hampshire.tweets <- all.tweets %>% 
-  filter(user_location == "New Hampshire")
-
-new.jersey.tweets <- all.tweets %>% 
-  filter(user_location == "New Jersey")
-
-new.mexico.tweets <- all.tweets %>% 
-  filter(user_location == "New Mexico")
-
-new.york.tweets <- all.tweets %>% 
-  filter(user_location == "New York")
-
-north.carolina.tweets <- all.tweets %>% 
-  filter(user_location == "North Carolina")
-
-south.carolina.tweets <- all.tweets %>% 
-  filter(user_location == "South Carolina")
-
-north.dakota.tweets <- all.tweets %>% 
-  filter(user_location == "North Dakota")
-
-south.dakota.tweets <- all.tweets %>% 
-  filter(user_location == "South Dakota")
-
-ohio.tweets <- all.tweets %>% 
-  filter(user_location == "Ohio")
-
-oklahoma.tweets <- all.tweets %>% 
-  filter(user_location == "Oklahoma")
-
-oregon.tweets <- all.tweets %>% 
-  filter(user_location == "Oregon")
-
-pennsylvania.tweets <- all.tweets %>% 
-  filter(user_location == "Pennsylvania")
-
-rhode.island.tweets <- all.tweets %>% 
-  filter(user_location == "Rhode Island")
-
-tennessee.tweets <- all.tweets %>% 
-  filter(user_location == "Tennessee")
-
-texas.tweets <- all.tweets %>% 
-  filter(user_location == "Texas")
-
-utah.tweets <- all.tweets %>% 
-  filter(user_location == "Utah")
-
-vermont.tweets <- all.tweets %>% 
-  filter(user_location == "Vermont")
-
-virginia.tweets <- all.tweets %>% 
-  filter(user_location == "Virginia")
-
-west.virginia.tweets <- all.tweets %>% 
-  filter(user_location == "West Virginia")
-
-washington.tweets <- all.tweets %>% 
-  filter(user_location == "Washington")
-
-wisconsin.tweets <- all.tweets %>% 
-  filter(user_location == "Wisconsin")
-
-wyoming.tweets <- all.tweets %>% 
-  filter(user_location == "Wyoming")
-
-all.states.tweets <- rbind(alabama.tweets, alaska.tweets, arizona.tweets, arkansas.tweets, california.tweets, colorado.tweets, connecticut.tweets, delaware.tweets, florida.tweets, georgia.tweets, hawaii.tweets, idaho.tweets, illinois.tweets, indiana.tweets, iowa.tweets, kansas.tweets, kentucky.tweets, louisiana.tweets, maine.tweets, maryland.tweets, massachussetts.tweets, michigan.tweets, minnesota.tweets, mississippi.tweets, missouri.tweets, montana.tweets, nebraska.tweets, nevada.tweets, new.hampshire.tweets, new.jersey.tweets, new.mexico.tweets, new.york.tweets, north.carolina.tweets, south.carolina.tweets, north.dakota.tweets, south.dakota.tweets, ohio.tweets, oklahoma.tweets, oregon.tweets, pennsylvania.tweets, rhode.island.tweets, tennessee.tweets, texas.tweets, utah.tweets, vermont.tweets, virginia.tweets, west.virginia.tweets, washington.tweets, wisconsin.tweets, wyoming.tweets)
-write.csv(all.states.tweets, "data/states.tweets.csv")
-
-#second, if text contains 'Trump'/'Hillary', add column 'Candidate' with either a 1 or a 2
-filtered.tweets <- mutate(all.states.tweets, Candidate = "-")
-
-DetermineCandidate <- function(tweets, row){
-  tweet.text <- tweets$text[row]
-  if(grepl("Trump", tweet.text) & !grepl("Clinton", tweet.text)){
-    tweets$Candidate[row] <- "Trump"
-  }else if(grepl("Clinton", tweet.text) & !grepl("Trump", tweet.text)){
-    tweets$Candidate[row] <- "Clinton"
-  }
-  return(tweets)
-}
-
-for(i in 1:nrow(filtered.tweets)){
-  filtered.tweets <- DetermineCandidate(filtered.tweets, i)
-}
-
-filtered.tweets <- filter(filtered.tweets, Candidate != "-")
-
-#third, group by Candidate column, then group by user_location, then summarise and find n()
-trump.tweets <- filter(filtered.tweets, Candidate == "Trump")
-clinton.tweets <- filter(filtered.tweets, Candidate == "Clinton")
-
-trump.location <- group_by(trump.tweets, user_location) %>% 
-  summarise(
-    frequency = n()
-  )
-clinton.location <- group_by(clinton.tweets, user_location) %>% 
-  summarise(
-    frequency = n()
-  )
-
-#fourth, merge with us map data
-combined <- sort(union(levels(usa.map$region), levels(trump.location$user_location)))
-n <- left_join(mutate(usa.map, region = factor(region, levels = combined)),
-               mutate(trump.location, user_location = factor(user_location, levels = combined)), by = c("region" = "user_location"))
-
-t.data.map <- left_join(usa.map, trump.location, by = c("region" = "user_location"))
-c.data.map <- left_join(usa.map, clinton.location, by = c("region" = "user_location"))
-
-#fifth, cut to create different groups?
-
-
-#sixth, create visualization
-ggplot(data = t.data.map) + 
-  geom_polygon(aes(x = long, y = lat, group = group, fill = t.frequency)) +
-  coord_quickmap() +
-  scale_fill_distiller(name="???", palette = "Set2", na.value = "grey50") +
-  labs(title="Trump")
-
-ggplot(data = c.data.map) + 
-  geom_polygon(aes(x = long, y = lat, group = group, fill = c.frequency)) +
-  coord_quickmap() +
-  scale_fill_distiller(name="???", palette = "Set2", na.value = "grey50") +
-  labs(title="Clinton")
+colnames(trump.location) <- c("State", "Number of Tweets")
+colnames(clinton.location) <- c("State", "Number of Tweets")
 
 my.server <- function(input, output){
-  filtered <- reactive({
-    #copy all the stuff from above here
-    
-  })
-
-
-  output$plot <- renderPlot({
-    usa.map <- map_data("state")
-
-    ggplot(data = data.map) + 
+  output$trump.map <- renderPlot({
+    p <- ggplot(data = t.data.map) + 
       geom_polygon(aes(x = long, y = lat, group = group, fill = frequency)) +
-      facet_wrap(~Candidate) +
       coord_quickmap() +
-      scale_fill_distiller(name="???", palette = "Set2", na.value = "grey50") +
-      labs(title="lol lol lol")
+      #scale_fill_distiller(name="???", palette = "Set2", na.value = "grey50") +
+      labs(title="Trump")
+    p
+  })
+  
+  output$trump.info <- renderPrint({
+    return(input$click.key1)
+  })
+  
+  output$t.per.state.table <- renderDataTable({
+    return(trump.location)
+  })
+  
+  output$clinton.map <- renderPlot({
     
+    p <- ggplot(data = c.data.map) + 
+      geom_polygon(aes(x = long, y = lat, group = group, fill = frequency)) +
+      coord_quickmap() +
+      #scale_fill_distiller(name="???", palette = "Set2", na.value = "grey50") +
+      labs(title="Clinton")
+    
+    p
+  })
+  
+  output$clinton.info <- renderPrint({
+    return(input$click.key2)
+  })
+  
+  output$c.per.state.table <- renderDataTable({
+    return(clinton.location)
   })
   
 }
--- ui.R
+++ ui.R
@@ -1,25 +1,25 @@
-library("dplyr")
-library("ggplot2")
-library("shiny")
+library(ggplot2) 
+library(tm) 
+library(SnowballC) 
+library(wordcloud) 
+library(RColorBrewer) 
+library(shinythemes) 
+library(plotly) 
+library(dplyr)
 
-all.tweets <- read.csv("data/tweets.csv")
+t.data.map <- read.csv("data/trump.state.map.csv")
+c.data.map <- read.csv("data/clinton.state.map.csv")
+trump.location <- read.csv("data/trump.state.freq.csv")
+clinton.location <- read.csv("data/clinton.state.freq.csv")
 
 my.ui <- fluidPage(
-  
-  titlePanel("'President' Trump lol lol lol lol lol lol"),
-  
-  sidebarLayout(
+  theme = shinythemes::shinytheme("united"),
+  navbarPage("Comparing Number of Tweets per State between Donald Trump and Hillary Clinton Pre-Election",
+    tabPanel('Tweets about Trump State Map', plotOutput('trump.map', click = 'click.key1'), textOutput('plo1t.description'), verbatimTextOutput('trump.info')),
+    tabPanel('Tweets about Clinton State Map', plotOutput('clinton.map', click = 'click.key2'), textOutput('plot2.description'), verbatimTextOutput('clinton.info')),
+    tabPanel('Tweets about Trump per State Table', dataTableOutput('t.per.state.table'), textOutput('table1.description')),
+    tabPanel('Tweets about Clinton per State Table', dataTableOutput('c.per.state.table'), textOutput('table2.description'))
     
-    sidebarPanel(
-      #checkboxGroupInput('candidates', label = "Choose Candidates", choices = c("Donald Trump", "Hilary Clinton", "Bernie Sanders"))
-      
-    ),
-    
-    mainPanel(
-      tabsetPanel(type = "tabs",
-        tabPanel('Plot', plotOutput('plot', click = 'click.key'))
-      )
-    )
   )
 )
 
