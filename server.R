fd0a6144fd1c0496f6d755c5166d4b551c674ee7
megan-vo <vo.meganb@gmail.com>
2017-03-06 21:40:20 -0800

    Add comments

--- server.R
+++ server.R
@@ -12,7 +12,10 @@ library(dplyr)
 # Data Frames #
 ###############
 
+# Set up data frame of 1050 tweets about candidates 
 sample.polarity <- read.csv("data/sample.polarity.tweets.csv", stringsAsFactors = F)
+
+# Set up data frames with random samples of #MAGA, #WomensMarch, and Women's March images
 maga <- read.csv("data/MAGAtweets.csv", stringsAsFactors = F) %>% 
   sample_n(300)
 w.march <- read.csv("data/womensmarchtweets.csv", stringsAsFactors = F) %>% 
@@ -23,8 +26,11 @@ march.photos <- read.csv("data/womensmarchphotos.csv", stringsAsFactors = F) %>%
 # Defines server function
 my.server <- function(input, output) {
   
+  # Creates reactive function that takes in user input and changes polarity tweets data frame to view
   polarity.candidate <- reactive({
     data <- sample.polarity
+    
+    # If the user checks one, two, or three checkboxes, filters according to who they checked
     if (length(input$polarity.data) == 1) {
       data <- filter(data, Candidate == input$polarity.data)
     } else if (length(input$polarity.data) == 2) {
@@ -32,19 +38,26 @@ my.server <- function(input, output) {
       data.other <- filter(sample.polarity, Candidate == input$polarity.data[2])
       data <- rbind(data, data.other)
     } 
+    
+    # Filters based on range of slider input
     data <- filter(data, polarity > input$polarity.range[1] & polarity < input$polarity.range[2])
     return(data)
   })
   
+  # Resets x and y bounds based on user zoom in/out
   ranges <- reactiveValues(x = NULL, y = NULL)
   
+  # Creates polarity plot
   output$polarity.plot <- renderPlot({
+    
+    # Creates histogram of polarity based on reactive data
     plot <- ggplot(polarity.candidate(), aes(polarity, fill = Candidate)) +
       geom_histogram(position = "dodge", binwidth = 0.1) +
       scale_fill_brewer(direction = -1) + 
       theme_minimal() +
-      coord_cartesian(xlim = ranges$x, ylim = ranges$y) 
+      coord_cartesian(xlim = ranges$x, ylim = ranges$y) # reset axes of plot based on zoom in/out
     
+    # Change the positioning of the histogram to stacked if user indicates
     if(input$stacked) {
       plot <- plot + geom_histogram(position = "stack")
     }
@@ -52,48 +65,62 @@ my.server <- function(input, output) {
     return(plot)
   })
   
+  # Interactivity of plot using double clicks and brushes
   observeEvent(input$polar_dblclick, {
     brush <- input$polar_brush
+    
+    # Set the plot's axes and zoom in if the user selects an area and double clicks
     if(!is.null(brush)) {
       ranges$x <- c(brush$xmin, brush$xmax)
       ranges$y <- c(brush$ymin, brush$ymax)
+      
+      # If the user double clicks again, zoom out
     } else {
       ranges$x <- NULL
       ranges$y <- NULL
     }
   })
   
+  # Generate analysis of polarity tweets based on data user is viewing 
   output$polarity.analysis <- renderText({
     data <- polarity.candidate()
 
+    # Grab the number of rows, max and min polarity, and the candidates those tweets are mentioning
     rows <- nrow(data)
     max <- max(data$polarity)
     max.candidate <- filter(data, polarity == max) 
     max.candidate <- unique(max.candidate$Candidate)
+    
+    # If there is more than one candidate with the max, print custom message
     if (length(max.candidate) == 3) {
       max.candidate <- "Bernie, Clinton, and Trump"
     } else if (length(max.candidate) == 2) {
       max.candidate <- max.candidate[1] + " and " + max.candidate[2]
     } 
     
+    # Find the min polarity and candidate
     min <- min(data$polarity)
     min.candidate <- filter(data, polarity == min) 
     min.candidate <- unique(min.candidate$Candidate)
     
+    # If multiple candidates have same min polarity, print custom message
     if (length(min.candidate) == 3) {
       min.candidate <- "Bernie, Clinton, and Trump"
     } else if (length(min.candidate) == 2) {
       min.candidate <- min.candidate[1] + " and " + min.candidate[2]
     }
     
+    # Find total average polarity of data
     avg <- data %>% 
       summarize(avg = mean(data$polarity))
     
+    # Find summary statistics of sample 350 of each candidate
     summary <- sample.polarity %>% 
       group_by(Candidate) %>% 
       summarize(mean = mean(polarity))
     
     
+    # Analysis of data based on reactive data
     analysis <- paste0("You are currently viewing ", rows, " tweets of 1050. The most positive score is ",
                       round(max, 2), ", which mentions the candidate(s) ", max.candidate, ". The most negative score is 
                       ", round(min, 2), ", and it is about the candidate(s) ", min.candidate, ". The current average
@@ -110,22 +137,26 @@ my.server <- function(input, output) {
     return(analysis)
   })
 
+  # Generate a new image url if action button is clicked
   img.url <- reactive({
     if (input$generate.img) {
       march.image <- march.photos %>% 
-        sample_n(1)
+        sample_n(1) #grab one image
       return(march.image)
     }
   })
   
+  # Generate random tweets
   output$maga.march.tweet <- renderPrint({
     
+    # If user presses action button, generate new random tweet from sample
     if(input$generate.march.tweet) {
       tweet.maga <- maga %>% 
         sample_n(1)
       tweet.march <- w.march %>% 
         sample_n(1)
 
+      # Format text of tweets
       tweet.maga <- paste("#MAGA: ", tweet.maga$text)
       tweet.march <- paste("#WomensMarch: ", tweet.march$text)
       cat(tweet.maga, tweet.march, sep = "\n\n")
@@ -133,18 +164,25 @@ my.server <- function(input, output) {
     
   })
   
+  # Generates new image based on either button or copy/paste url
   output$image <- renderUI({
+    
+    # If user chooses to hit action button, generate new random image
     if(input$image_url == "http://placehold.it/300x300" || input$image_url == "") {
       tags$img(src = img.url()$image, width = 400, height = 400) 
+      
+    # If user chooses to copy paste, generates chosen url image
     } else {
       tags$img(src = input$image_url, width = 400, height = 400)
     }
   })
   
+  # Prints out image URL to shiny page
   output$url <- renderPrint({
     cat("URL: ", img.url()$image)
   })
   
+  # List out favorite image urls
   output$url.text <- renderUI({
     list <- tags$ul(
       tags$li("http://pbs.twimg.com/media/C2u4QQvWEAAqLpM.jpg"),
@@ -152,8 +190,9 @@ my.server <- function(input, output) {
       tags$li("http://pbs.twimg.com/media/C2u2yWRVQAAXTMx.jpg"),
       tags$li("http://pbs.twimg.com/media/C2u1HdaUAAAfNUR.jpg")
     )
-    HTML(paste("", list, ""), sep = "</br>")
+    HTML(paste("", list, ""), sep = "</br>") # separate the list from the paragraph text with break line
   })
 }
 
+# Creates server 
 shinyServer(my.server)
--- ui.R
+++ ui.R
@@ -13,19 +13,20 @@ my.ui <- fluidPage(
   theme = shinythemes::shinytheme("united"),
   navbarPage("Navbar",
              
+
              # Navbar1
-             tabPanel(
-               
+             tabPanel("Foo"
+                      
              ),
              
              # Navbar2
-             tabPanel(
-               
+             tabPanel("Bar"
+                      
              ),
              
              # Navbar3
-             tabPanel(
-               
+             tabPanel("Baz"
+                      
              ),
              
     # Navigation bar to polarity of tweets data        
@@ -96,6 +97,7 @@ my.ui <- fluidPage(
     # Navbar to Bonus page
     tabPanel("Bonus: #MAGA, #WomensMarch",
       
+      # Title and description of bonus page examining MAGA and Women's March tweets
       tabPanel("Extra: MAGA and the Women's March Tweets",
                h3("Comparing #MAGA and #WomensMarch"),
                h5(strong( "Did We Really Make America Great Again?") ),
@@ -103,41 +105,57 @@ my.ui <- fluidPage(
                  strong("#MAGA and #WomensMarch "), "tweets side-by-side from Jan. 20th & 21st. It's more of 
                  a little qualitative look into Twitter data related to Trump and the Women's March."),
                
+               # Instructions of page and how to view tweets/img
                p("Go ahead - hit the", strong( em("'Compare' ")), "button. A random tweet from each 
-                 hashtag will pop up along with a randomly generated photo (separate from the tweets)
-                 from the Women's March. Next, ", strong( em("copy and paste ") ), "the url. While we do want
-                 to warn that", strong(em(" viewer discretion is advised, ")), "we also want you to have fun
+                 hashtag will pop up. If you hit the", strong( em (' Generate')), " button, it will generate a 
+                 random photo from the Women's March twitter data set (which is separate from the tweets). While we 
+                 do want to warn that", strong(em(" viewer discretion is advised, ")), "we also want you to have fun
                  perusing through!", em(" - Love the Creative Group")),
                
                p("Data derived from: Wendy He's #MAGA and #WomensMarch Data set: ",
                  em(a("https://data.world/wendyhe/tweets-on-womensmarch-and-maga"))),
-                             
-                            
+      
+                                      
+     # Widgets for the bonus section tweets and images                   
      fluidRow(
+       
+       # Adds action button to generate tweets
        column(6,
               actionButton('generate.march.tweet', label = "Compare #MAGA & #WM Tweets", width = 300,
                            icon = icon("retweet", lib = "font-awesome"), class = "btn-primary"),
+              
+              # Generates random tweets into shiny page
               verbatimTextOutput('maga.march.tweet'),
+              
+              # Adds copy/paste img url option below tweets action button and to side of image
               fluidRow(
                 column(9, 
                        h4(strong("Check Out Some of Our Favorite Images: ")),
                        p("We've selected some of our favorite random pictures from the data set
                          for your viewing pleasure. Just ", strong(em("copy and paste")), " any of 
                          the following urls below."),
-                      htmlOutput("url.text"),
+                       
+                       # Outputs the selected url favorites
+                       htmlOutput("url.text"),
                        p("Just remember that once you are done viewing, please",
                          strong( em( " delete the url ") ), "from the input.
                          Happy clicking!"),
                        
+                       # Text input widget for url
                        textInput("image_url",
                                  label = 'Paste Image URL',
                                  value = "http://placehold.it/300x300"))
                 )
               ),
        
+       # Generates image in right column
        column(5,
+              
+              # Makes generating action button for images at random
               actionButton('generate.img', label = "Generate Random Image from Women's March", width = 400,
                            icon = icon("retweet", lib = "font-awesome"), class = "btn-secondary"),
+              
+              # Outputs image and image url
               htmlOutput("image"),
               verbatimTextOutput("url"))
        )
@@ -147,5 +165,5 @@ my.ui <- fluidPage(
   )
 )  
 
-
+# Make UI from my.ui
 shinyUI(my.ui)
