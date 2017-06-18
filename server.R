4faa8c230868856d3f1eb5e8308db93e4904d7a8
megan-vo <vo.meganb@gmail.com>
2017-06-18 15:23:00 -0700

    Omit word analysis portion

--- server.R
+++ server.R
@@ -31,33 +31,33 @@ candidate.tweets <- read.csv("data/trump-clinton-tweets.csv")
 candidate.tweets$candidate[candidate.tweets$handle == "HillaryClinton"] <- "Hillary Clinton"
 candidate.tweets$candidate[candidate.tweets$handle=="realDonaldTrump"] <- "Donald Trump"
 
-# Reads in data set for word analysis
-data <- read.csv("data/Crowdbabble_Social-Media-Analytics_Twitter-Download_Donald-Trump_7375-Tweets.csv")
-
-# Filtering data for tweet text
-corpus <- Corpus(VectorSource(data$Tweet_Text))
-
-# Text Processing to isolate text
-corpus <- tm_map(corpus, removePunctuation)
-corpus <- tm_map(corpus, removeNumbers)
-corpus <- tm_map(corpus, tolower)
-corpus <- tm_map(corpus, function(x) iconv(x, 'UTF-8', 'ASCII'))
-corpus <- tm_map(corpus, removeWords, stopwords(kind = "en"))
-corpus <- tm_map(corpus, stripWhitespace)
-corpus <- tm_map(corpus, tolower) 
-corpus <- tm_map(corpus, function(x) iconv(x, 'UTF-8', "ASCII"))
-corpus <- tm_map(corpus, removeWords, stopwords(kind = "en")) 
-corpus <- tm_map(corpus, stripWhitespace) 
-corpus <- tm_map(corpus, removeWords, c("will", "thank", "realdonaldtrump", "amp",
-                                        "just", "people", "get", "now", "like", "new",
-                                        "back", "dont", "much"))
-# THIS IS FOR THE FREQ PLOT
-# Creates document term matrix
-dtm <- DocumentTermMatrix(corpus)
-# Organize terms by their frequency
-freq <- colSums(as.matrix(dtm))
-# This will identify all terms that appear frequently in a data frame
-wf <- data.frame(word=names(freq), freq=freq)
+# # Reads in data set for word analysis
+# data <- read.csv("data/Crowdbabble_Social-Media-Analytics_Twitter-Download_Donald-Trump_7375-Tweets.csv")
+# 
+# # Filtering data for tweet text
+# corpus <- Corpus(VectorSource(data$Tweet_Text))
+# 
+# # Text Processing to isolate text
+# corpus <- tm_map(corpus, removePunctuation)
+# corpus <- tm_map(corpus, removeNumbers)
+# corpus <- tm_map(corpus, tolower)
+# corpus <- tm_map(corpus, function(x) iconv(x, 'UTF-8', 'ASCII'))
+# corpus <- tm_map(corpus, removeWords, stopwords(kind = "en"))
+# corpus <- tm_map(corpus, stripWhitespace)
+# corpus <- tm_map(corpus, tolower) 
+# corpus <- tm_map(corpus, function(x) iconv(x, 'UTF-8', "ASCII"))
+# corpus <- tm_map(corpus, removeWords, stopwords(kind = "en")) 
+# corpus <- tm_map(corpus, stripWhitespace) 
+# corpus <- tm_map(corpus, removeWords, c("will", "thank", "realdonaldtrump", "amp",
+#                                         "just", "people", "get", "now", "like", "new",
+#                                         "back", "dont", "much"))
+# # THIS IS FOR THE FREQ PLOT
+# # Creates document term matrix
+# dtm <- DocumentTermMatrix(corpus)
+# # Organize terms by their frequency
+# freq <- colSums(as.matrix(dtm))
+# # This will identify all terms that appear frequently in a data frame
+# wf <- data.frame(word=names(freq), freq=freq)
 
 #Loads data sets for analysis from data folder
 t.data.map <- read.csv("data/trump.state.map.csv")
@@ -388,35 +388,35 @@ my.server <- function(input, output) {
   
   )
   
-  #################
-  # Word Analysis #
-  #################
-  # Creates Reactive variable for the wordcloud
-  terms <- reactive({
-    myDTM = TermDocumentMatrix(corpus, control = list(minWordLength = 1))
-    m = as.matrix(myDTM)
-    sort(rowSums(m), decreasing = TRUE)
-  })
-
-  # Make the wordcloud drawing predictable during a session
-  wordcloud_rep <- repeatable(wordcloud)
-
-  # Creates Output for Word Cloud
-  output$plot.cloud <- renderPlot({
-    v <- terms()
-    wordcloud_rep(names(v), v, scale=c(6,0.5),
-                  min.freq = input$freq, max.words=input$max,
-                  colors=brewer.pal(8, "Dark2"))
-  })
-
-  # Creates output for Frequency Plot
-  output$graph <- renderPlotly({
-    p <- ggplot(subset(wf, freq>350), aes(word, freq))
-    p <- p + geom_bar(stat="identity")
-    p <- p + theme(axis.text.x=element_text(angle=45, hjust=1))
-    p <- p + labs(x = "Word(s)", y = "Frequency")
-    p
-  })
+  # #################
+  # # Word Analysis #
+  # #################
+  # # Creates Reactive variable for the wordcloud
+  # terms <- reactive({
+  #   myDTM = TermDocumentMatrix(corpus, control = list(minWordLength = 1))
+  #   m = as.matrix(myDTM)
+  #   sort(rowSums(m), decreasing = TRUE)
+  # })
+  # 
+  # # Make the wordcloud drawing predictable during a session
+  # wordcloud_rep <- repeatable(wordcloud)
+  # 
+  # # Creates Output for Word Cloud
+  # output$plot.cloud <- renderPlot({
+  #   v <- terms()
+  #   wordcloud_rep(names(v), v, scale=c(6,0.5),
+  #                 min.freq = input$freq, max.words=input$max,
+  #                 colors=brewer.pal(8, "Dark2"))
+  # })
+  # 
+  # # Creates output for Frequency Plot
+  # output$graph <- renderPlotly({
+  #   p <- ggplot(subset(wf, freq>350), aes(word, freq))
+  #   p <- p + geom_bar(stat="identity")
+  #   p <- p + theme(axis.text.x=element_text(angle=45, hjust=1))
+  #   p <- p + labs(x = "Word(s)", y = "Frequency")
+  #   p
+  # })
 
   #################
   # Map of Tweets #
--- ui.R
+++ ui.R
@@ -97,7 +97,9 @@ my.ui <- fluidPage(
                         h3("List of Data Sets We Used"),
                         p("Trump vs. Clinton Tweets' Faves and Retweets: ", a("https://www.kaggle.com/benhamner/clinton-trump-tweets")),
                         p("Tweets about Candidates for Map and Polarity: ", a("https://www.dataquest.io/blog/matplotlib-tutorial/")),
-                        p("Trump Tweets for Word Analysis: ", a("https://www.crowdbabble.com/blog/the-11-best-tweets-of-all-time-by-donald-trump/")),
+                        
+                        #p("Trump Tweets for Word Analysis: ", a("https://www.crowdbabble.com/blog/the-11-best-tweets-of-all-time-by-donald-trump/")),
+                        
                         p("#MAGA & #WomensMarch Tweets/Images: ", a("https://data.world/wendyhe/tweets-on-womensmarch-and-maga"))
                         )
     ),
@@ -188,65 +190,65 @@ my.ui <- fluidPage(
     
     
     
-    tabPanel("Word Analysis",
-             # Creates side panel
-             sidebarPanel(
-               # Min Freq Slider Widget
-               sliderInput("freq",
-                           "Minimum Frequency",
-                           min = 50,  max = 1000, value = 100),
-               # Max words Slider widget
-               sliderInput("max",
-                           "Maximum Number of Words",
-                           min = 1,  max = 300,  value = 100)
-             ),
-             
-             # Creates Main Panel
-             mainPanel(
-               tabsetPanel(
-                 # Creates Word Cloud Tab
-                 tabPanel("Word Cloud",
-                          h3('Trump\'s Tweets Word Frequency Cloud'),
-                          p('The purpose of this section is to look at the',
-                            em("word choice"), 'and the', em('frequency'), 
-                            'of that word choice with regards to over', strong('7,000'), 'of Donald Trump\'s', 
-                            strong('tweets.'),
-                            'This is displayed by a', em('word cloud '), ' 
-                            in which the words are colorized for visual diferentiation and
-                            will display as larger if they are more frequently used.'),
-                          p('', strong('Functionalities:'), 'You are able to 
-                            filter for the minimum amount of times a word appears in a set of tweets 
-                            as well as filter for the maximum amount of words in the cloud via the
-                            widgets on the side.'),
-                          plotOutput("plot.cloud", width = "100%"),
-                          p(strong("Observations:"), "By looking at the word cloud it is easy to see that the largest and most frequent word by far
-                            is, well, Trump. This is due to the amount of times Trump quoted a tweet with his own name in it. This goes to show that 
-                            our President must be", em("very "), " concerned with his public perception. It also shows that his word choice is
-                            usually rather blunt and abrasive, one that requires a good amount of imagination to take any relevant or coherent meaning from it."),
-                          p('The data set for both the cloud and the plot was derived from - https://www.crowdbabble.com/blog/the-11-best-tweets-of-all-time-by-donald-trump/')
-                          ),
-                 # Creates Frequency Plot Tab
-                 tabPanel("Frequency Plot",
-                          h3("Trump\'s Tweets Word Frequency Bar Plot"),
-                          p('The purpose of this section is to look at the most frequently 
-                            occuring words in Trump\'s tweets and their frequency.'),
-                          p('', strong('Functionalities:'), 'You are able to 
-                            hover over each bar of the graph to get specific information.'),
-                          plotlyOutput("graph"),
-                          p(strong("Observations:"), "By looking at the word cloud it is easy to see that the largest and most frequent word by far
-                            is, well, Trump. This is due to the amount of times Trump quoted a tweet with his own name in it. This goes to show that 
-                            our President must be", em("very "), " concerned with his public perception. It also shows that his word choice is
-                            usually rather blunt and abrasive, one that requires a good amount of imagination to take any relevant or coherent meaning from it."),
-                          p('The data set for both the cloud and the plot was derived from - https://www.crowdbabble.com/blog/the-11-best-tweets-of-all-time-by-donald-trump/')
-                          )
-                 
-                 
-                          )
-               
-               
-                 )
-             
-                 ),
+    # tabPanel("Word Analysis",
+    #          # Creates side panel
+    #          sidebarPanel(
+    #            # Min Freq Slider Widget
+    #            sliderInput("freq",
+    #                        "Minimum Frequency",
+    #                        min = 50,  max = 1000, value = 100),
+    #            # Max words Slider widget
+    #            sliderInput("max",
+    #                        "Maximum Number of Words",
+    #                        min = 1,  max = 300,  value = 100)
+    #          ),
+    #          
+    #          # Creates Main Panel
+    #          mainPanel(
+    #            tabsetPanel(
+    #              # Creates Word Cloud Tab
+    #              tabPanel("Word Cloud",
+    #                       h3('Trump\'s Tweets Word Frequency Cloud'),
+    #                       p('The purpose of this section is to look at the',
+    #                         em("word choice"), 'and the', em('frequency'), 
+    #                         'of that word choice with regards to over', strong('7,000'), 'of Donald Trump\'s', 
+    #                         strong('tweets.'),
+    #                         'This is displayed by a', em('word cloud '), ' 
+    #                         in which the words are colorized for visual diferentiation and
+    #                         will display as larger if they are more frequently used.'),
+    #                       p('', strong('Functionalities:'), 'You are able to 
+    #                         filter for the minimum amount of times a word appears in a set of tweets 
+    #                         as well as filter for the maximum amount of words in the cloud via the
+    #                         widgets on the side.'),
+    #                       plotOutput("plot.cloud", width = "100%"),
+    #                       p(strong("Observations:"), "By looking at the word cloud it is easy to see that the largest and most frequent word by far
+    #                         is, well, Trump. This is due to the amount of times Trump quoted a tweet with his own name in it. This goes to show that 
+    #                         our President must be", em("very "), " concerned with his public perception. It also shows that his word choice is
+    #                         usually rather blunt and abrasive, one that requires a good amount of imagination to take any relevant or coherent meaning from it."),
+    #                       p('The data set for both the cloud and the plot was derived from - https://www.crowdbabble.com/blog/the-11-best-tweets-of-all-time-by-donald-trump/')
+    #                       ),
+    #              # Creates Frequency Plot Tab
+    #              tabPanel("Frequency Plot",
+    #                       h3("Trump\'s Tweets Word Frequency Bar Plot"),
+    #                       p('The purpose of this section is to look at the most frequently 
+    #                         occuring words in Trump\'s tweets and their frequency.'),
+    #                       p('', strong('Functionalities:'), 'You are able to 
+    #                         hover over each bar of the graph to get specific information.'),
+    #                       plotlyOutput("graph"),
+    #                       p(strong("Observations:"), "By looking at the word cloud it is easy to see that the largest and most frequent word by far
+    #                         is, well, Trump. This is due to the amount of times Trump quoted a tweet with his own name in it. This goes to show that 
+    #                         our President must be", em("very "), " concerned with his public perception. It also shows that his word choice is
+    #                         usually rather blunt and abrasive, one that requires a good amount of imagination to take any relevant or coherent meaning from it."),
+    #                       p('The data set for both the cloud and the plot was derived from - https://www.crowdbabble.com/blog/the-11-best-tweets-of-all-time-by-donald-trump/')
+    #                       )
+    #              
+    #              
+    #                       )
+    #            
+    #            
+    #              )
+    #          
+    #              ),
     
     # Navbar3
     tabPanel("Map of Tweets about Candidates",
